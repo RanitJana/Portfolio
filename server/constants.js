86f0e62ae14b94e7504098bf7530bc5d5a6f6814
Ranit Jana <ranitjana100@gmail.com>
2024-10-02 01:20:51 +0530

    fix:cookie settings for production

--- server/constants.js
+++ server/constants.js
@@ -1,10 +1,12 @@
 const DB_NAME = "Portfolio";
 const cookieOptions = {
-  maxAge: 1000 * 60 * 60 * 24 * 7,
-  httpOnly: true,        // Prevent access via JavaScript
-  secure: true,          // Ensures the cookie is sent over HTTPS
-  sameSite: 'none',
+  httpOnly: true,           // Prevents JavaScript access to cookies (recommended for session/auth cookies)
+  secure: true,             // Ensures cookies are sent only over HTTPS
+  sameSite: 'None',         // Required for cross-site cookies; must be 'None' for cookies to work across domains
+  path: '/',                // Makes cookies available site-wide
+  maxAge: 24 * 60 * 60 * 1000, // Set cookie expiration (e.g., 1 day in milliseconds)
 };
+
 
 
 const _envValue = Object.freeze({
