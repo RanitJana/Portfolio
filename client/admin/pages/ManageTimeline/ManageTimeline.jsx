1e879881d411938bfc34cfc181c0f10d9312c5e8
Ranit Jana <ranitjana100@gmail.com>
2024-10-06 11:50:49 +0530

    feat:start project section

--- client/admin/pages/ManageTimeline/ManageTimeline.jsx
+++ client/admin/pages/ManageTimeline/ManageTimeline.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-unused-vars */
 import "./ManageTimeline.css";
 import TimelineCard from "../../components/TimelineCard/TimelineCard";
 import { handleTimeline } from "../../../src/utils/Apis.js";
--- client/admin/pages/ProjectCreate/ProjectCreate.jsx
+++ client/admin/pages/ProjectCreate/ProjectCreate.jsx
@@ -1,7 +1,14 @@
 import "./ProjectCreate.css";
 
 function ProjectCreate() {
-  return <div>ProjectCreate</div>;
+  return (
+    <div className="projectCreate">
+      <div className="top">
+        <h2>Projects</h2>
+        <button>Add</button>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCreate;
